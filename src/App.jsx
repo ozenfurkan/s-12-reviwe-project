21f5ced34ffb1d6c57789e11b3eff8cfba1863da
ozenfurkan <furkanzihniozen1@gmail.com>
2024-07-20 17:34:50 +0300

    tailwind css kuruldu.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function App() {
+  return (
+    <>
+      <div className="dark">
+        <h1 className="text-3xl font-bold underline dark:bg-slate-600">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  );
+}
